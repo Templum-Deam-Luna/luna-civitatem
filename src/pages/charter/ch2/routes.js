99c5635ea1eaebd2a7a3e86e218a2d0327710480
Sam Grundman <samuel@yodas.ws>
2017-12-07 09:27:35 -0500

    Create routes.js

---
+++ src/pages/charter/ch2/routes.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('pageCharterCh2')
+.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/charter/ch2/', {
+		templateUrl: 'pages/charter/ch2/ch2.html',
+		controllerAs: '$ctrl',
+		controller() {
+		},
+	})
+}])
